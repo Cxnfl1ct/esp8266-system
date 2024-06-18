e510b80f1185bc1941ed4bad2dfdadd6a1e36526
Cxnfl1ct <121840458+Cxnfl1ct@users.noreply.github.com>
2024-06-18 07:28:24 +0000

    Create Settings.h

---
+++ Settings.h
@@ -0,0 +1,11 @@
+/*
+A file for settings
+*/
+
+// Settings
+
+const char *ssid = "<ssid>";
+const char *password = "<password>";
+
+int timeZone = 9; // Change this to your timezone
+int NTPUpdateInterval = 15;
